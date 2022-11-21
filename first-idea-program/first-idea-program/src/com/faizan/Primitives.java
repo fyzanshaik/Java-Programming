a09539b071f222ab1a389c4afe2c6bd6e46242af
Faizan Shaik <fyzan.shaik@gmail.com>
2022-11-21 23:06:05 +0530

    Created repo

---
+++ first-idea-program/first-idea-program/src/com/faizan/Primitives.java
@@ -0,0 +1,17 @@
+package com.faizan;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 239203.3243423;
+        long largeIntegervalues = 832984392324232L;
+        boolean check = true;
+
+        //String is not primitive
+        String name = "Faizan";
+
+
+    }
+}
